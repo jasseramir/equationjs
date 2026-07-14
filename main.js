2aa560f31b764c1362880f2e80c18a5736c89825
jasseramir <jasser.amir.work@gmail.com>
2026-07-15 02:23:07 +0300

    change console.log with return

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ class LinearEquation {
         this.equation = equationStr;
         this.variable = '';
         this.ref = [];
-        
+
         this.detectVar();
         this.parse();
         this.simplify();
     }
-    
+
     detectVar() {
         for (const char of this.equation) {
             if (/[a-zA-Z]/.test(char)) {
@@ -19,41 +19,41 @@ class LinearEquation {
             }
         }
     }
-    
+
     parseTerm(term, isRightSide) {
         if (term.startsWith('+')) {
             term = term.slice(1);
         }
-        
+
         if (term === this.variable) {
             term = '1' + this.variable;
         } else if (term === '-' + this.variable) {
             term = '-1' + this.variable;
         }
-        
+
         if (!term) return;
         const lastChar = term[term.length - 1];
-                
+
         if (lastChar === this.variable) {
             const value = Number(term.slice(0, -1));
-                    
+
             this.ref.push({
                 type: 'variable',
                 coefficient: isRightSide ? -value : value,
             });
         } else {
             const value = Number(term);
-                    
+
             this.ref.push({
                 type: 'number',
                 value: isRightSide ? -value : value,
             });
         }
     }
-    
+
     parseSide(side, isRightSide) {
         let currentTerm = '';
-        
+
         for (const char of side) {
             if (char === '-' || char === '+') {
                 this.parseTerm(currentTerm, isRightSide);
@@ -62,24 +62,24 @@ class LinearEquation {
                 currentTerm += char;
             }
         }
-        
+
         if (currentTerm !== '') {
             this.parseTerm(currentTerm, isRightSide);
         }
     }
-    
+
     parse() {
         const [leftSide, rightSide] = this.equation.replace(/\s+/g, '').split('=');
         this.ref = [];
-        
+
         this.parseSide(leftSide, false);
         this.parseSide(rightSide, true);
     }
-    
+
     simplify() {
         this.a = 0;
         this.b = 0;
-        
+
         for (const term of this.ref) {
             if (term?.type === 'variable') {
                 this.a += term?.coefficient;
@@ -87,24 +87,22 @@ class LinearEquation {
                 this.b += term?.value;
             }
         }
-        
+
         this.b = -this.b;
     }
-    
+
     solve() {
         if (this.a === 0 && this.b === 0) {
-            console.log('Infinite Solutions');
-            return;
+            return 'Infinite Solutions';
         }
-        
+
         if (this.a === 0) {
-            console.log('No Solution');
-            return;
+            return 'No Solution';
         }
-        
-        console.log(`${this.variable} = ${this.b / this.a}`)
+
+        return `${this.variable} = ${this.b / this.a}`;
     }
 }
 
 const test = new LinearEquation("2x + 5 = 11");
-test.solve();
+console.log(test.solve());
